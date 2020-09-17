7cc49a75d51f5fef768b1dee5ac31bb70d90a937
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2020-09-17 11:24:23 +0200

    Add SocialUser model

--- nens_auth_client/models.py
+++ nens_auth_client/models.py
@@ -1,4 +1,25 @@
 # (c) Nelen & Schuurmans.  Proprietary, see LICENSE file.
-# from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.postgres.fields import JSONField
+from django.core.exceptions import PermissionDenied
+from django.db import IntegrityError
+from django.db import models
 
-# Create your models here.
+
+user_model = getattr(settings, "AUTH_USER_MODEL", None) or "auth.User"
+
+
+class SocialUser(models.Model):
+    """Associates an external user (by uid) with a local user"""
+
+    user = models.ForeignKey(
+        user_model, related_name="social_user", on_delete=models.CASCADE
+    )
+    uid = models.CharField(max_length=255, db_index=True)
+    extra_data = JSONField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.uid
