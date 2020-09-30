3a1c667c1012e0d923b8deeef775a5304e29c449
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2020-09-30 15:38:14 +0200

    Add the middleware to the testsettings

--- nens_auth_client/testsettings.py
+++ nens_auth_client/testsettings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = (
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "nens_auth_client.middleware.AccessTokenMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 )
