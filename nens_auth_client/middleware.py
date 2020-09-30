51b2aabb3431b1aba218b788274c587b81266c68
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2020-09-30 09:29:36 +0200

    Middleware + test

--- nens_auth_client/middleware.py
+++ nens_auth_client/middleware.py
@@ -1,33 +1,35 @@
-from django.contrib.auth import authenticate
+from .backends import create_remoteuser
 from authlib.jose import jwt
-from django.conf import settings
-import requests
 from authlib.jose.errors import JoseError
+from django.conf import settings
+
+import django.contrib.auth as django_auth
+import requests
 
 
-def verify_token(token):
+def decode_token(token):
+    """Decode and validate an ID or Access token and return its payload.
+
+    Args:
+      token (str): JWT as a base64-encoded string
+
+    Returns:
+      claims (dict or None): the token payload
+
+    Raises:
+      authlib.jose.errors.JoseError if token is invalid
+    """
     jwks = requests.get(settings.NENS_AUTH_JWKS_URI)
-    try:
-        claims = jwt.decode(
-            token,
-            jwks.json(),
-            claims_options={
-                "iss": {
-                    "essential": True,
-                    "value": settings.NENS_AUTH_ISSUER
-                },
-                "aud": {
-                    "essential": True,
-                    "value": settings.NENS_AUTH_CLIENT_ID
-                },
-                "sub": {
-                    "essential": True
-                },
-            }
-        )
-        claims.validate()
-    except JoseError:
-        return
+    claims = jwt.decode(
+        token,
+        jwks.json(),
+        claims_options={
+            "iss": {"essential": True, "value": settings.NENS_AUTH_ISSUER},
+            "aud": {"essential": True, "value": settings.NENS_AUTH_CLIENT_ID},
+            "sub": {"essential": True},
+        },
+    )
+    claims.validate()
     return dict(claims)
 
 
@@ -42,25 +44,45 @@ class OAuth2TokenMiddleware:
     If there is already a user set (probably via a session cookie), the Bearer
     token is ignored.
     """
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # do something only if request contains a Bearer token
-        assert hasattr(request, 'user'), (
+        assert hasattr(request, "user"), (
             "The OAuth2TokenMiddleware middleware requires authentication "
             "middleware to be installed. Edit your MIDDLEWARE setting to "
             "insert 'django.contrib.auth.middleware.AuthenticationMiddleware'"
             "before 'nens_auth_client.middleware.OAuth2TokenMiddleware'."
         )
-        if (
+        # Do something only if there is a Bearer token AND there is no user.
+        if not (
             request.META.get("HTTP_AUTHORIZATION", "").startswith("Bearer")
             and (not hasattr(request, "user") or request.user.is_anonymous)
         ):
-            userinfo = verify_token(request.META["HTTP_AUTHORIZATION"][7:])
-            if userinfo:
-                user = authenticate(request=request, userinfo=userinfo)
-                if user:
-                    request.user = user
+            return self.get_response(request)
+
+        # See https://tools.ietf.org/html/rfc6750#section-2.1,
+        # Bearer is case-sensitive and there is exactly 1 seperator after.
+        token = request.META["HTTP_AUTHORIZATION"][7:]
+        try:
+            claims = decode_token(token)
+        except JoseError:
+            # do nothing
+            return self.get_response(request)
+
+        # The django authentication backend(s) should find a local user
+        user = django_auth.authenticate(request, claims=claims)
+
+        if user is None:
+            # do nothing
+            return self.get_response(request)
+
+        # Log the user in
+        request.user = user
+
+        # Create a permanent association between local and external users
+        if settings.NENS_AUTH_AUTO_CREATE_REMOTE_USER:
+            create_remoteuser(user, claims)
 
         return self.get_response(request)
--- nens_auth_client/tests/test_middleware.py
+++ nens_auth_client/tests/test_middleware.py
@@ -1,16 +1,34 @@
-import pytest
-from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
-
 from nens_auth_client.middleware import OAuth2TokenMiddleware
 
+import pytest
 
-def test_oauth2_middleware(id_token_generator, rf, rq_mocker, jwks):
-    id_token = id_token_generator()
-    request = rf.get(
-        "/", HTTP_AUTHORIZATION="Bearer " + id_token,
-    )
-    request.user = AnonymousUser()
+
+UserModel = get_user_model()
+
+
+@pytest.fixture
+def middleware_mocker(mocker, rq_mocker, jwks, settings):
+    """Mock necessary functions and create an authorization request"""
     # Mock the call to the external jwks
     rq_mocker.get(settings.NENS_AUTH_JWKS_URI, json=jwks)
-    processed_request = OAuth2TokenMiddleware(lambda x: x)(request)
+    # Mock the user association call
+    authenticate = mocker.patch("django.contrib.auth.authenticate")
+    authenticate.return_value = UserModel(username="testuser")
+    # Disable automatic RemoteUser creation
+    settings.NENS_AUTH_AUTO_CREATE_REMOTE_USER = False
+    return {"authenticate": authenticate}
+
+
+def test_oauth2_middleware(rf, id_token_generator, middleware_mocker, settings):
+    token = id_token_generator(sub="abcd")
+    request = rf.get("/", HTTP_AUTHORIZATION="Bearer " + token)
+    request.user = AnonymousUser()
+
+    processed = OAuth2TokenMiddleware(lambda x: x)(request)
+    assert processed.user.username == "testuser"
+
+    assert middleware_mocker["authenticate"].called
+    claims = middleware_mocker["authenticate"].call_args[1]["claims"]
+    assert claims["sub"] == "abcd"
