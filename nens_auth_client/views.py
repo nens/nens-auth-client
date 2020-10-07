6da3706697444ca1aeb2c6e6cd63b5df09422269
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2020-10-07 16:49:53 +0200

    Document error handling in authorize view

--- nens_auth_client/views.py
+++ nens_auth_client/views.py
@@ -79,7 +79,14 @@ def authorize(request):
 
     This is the callback url (a.k.a. redirect_uri) from the login view.
 
-    TODO: Gracefully handle errors (instead of bare 403 / 500)
+    Raises:
+    - ``authlib.jose.errors.JoseError``: cryptographic errors
+      The error details must never be shown to the user.
+    - ``authlib.integrations.base_client.errors.OAuthError``: OAuth2 errors.
+      These are defined in https://tools.ietf.org/html/rfc6749#section-4.1.2.1.
+      The error descriptions can be shown to the user.
+    - ``django.core.exceptions.PermissionDenied``: authorization errors.
+      This error is raises when no user is present to log in.
     """
     client = get_oauth_client()
     client.check_error_in_query_params(request)
