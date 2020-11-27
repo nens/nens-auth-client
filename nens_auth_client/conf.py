1a13ba3d53eeaf83f95fe19fbc520eee04900a18
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2020-11-27 14:40:42 +0100

    Reverts

--- nens_auth_client/conf.py
+++ nens_auth_client/conf.py
@@ -8,7 +8,6 @@ class NensAuthClientAppConf(AppConf):
     URL_NAMESPACE = "nens_auth_client:"  # prefixed to viewnames in reverse()
     TIMEOUT = 10  # Timeout for token, JWKS and discovery requests (seconds)
     LEEWAY = 120  # Amount of seconds that a token's expiry can be off
-    USERNAME_CLAIM = "cognito:username"  # The ID token claim that matches the Django username
 
     DEFAULT_SUCCESS_URL = "/"  # Default redirect after successful login
     DEFAULT_LOGOUT_URL = "/"  # Default redirect after successful logout
