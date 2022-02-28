8e1b4c9d80fab5f9502d02385e1b6ecfadf09979
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2022-02-28 17:02:56 +0100

    Finish

--- nens_auth_client/requests_session.py
+++ nens_auth_client/requests_session.py
@@ -1,11 +1,16 @@
-from requests import Session
 from .models import RemoteUser
 from .oauth import get_oauth_client
+from requests import Session
 
 
 def refresh_token(remote_user: RemoteUser):
     client = get_oauth_client()
-    client.fetch_access_token(refresh_token=remote_user.refresh_token, grant_type="refresh_token")
+    tokens = client.fetch_access_token(
+        refresh_token=remote_user.refresh_token, grant_type="refresh_token"
+    )
+    remote_user.id_token = tokens["id_token"]
+    remote_user.access_token = tokens["access_token"]
+    remote_user.save()
 
 
 class OAuth2Session(Session):
@@ -19,14 +24,17 @@ class OAuth2Session(Session):
         valid_seconds: if the access_token is valid less than this many seconds then
             the token will be refreshed
         **kwargs: see to authlib.integrations.requests_client.OAuth2Session and
-            then to requests.Session.    
+            then to requests.Session.
     """
+
     def __init__(self, remote_user: RemoteUser, **kwargs):
         def update_token_on_request(r, *args, **kwargs):
             if r.status_code == 401 and not getattr(r.request, "refresh_done", False):
                 # Refresh the token
                 refresh_token(remote_user)
-                self.headers.update({"Authorization": f"Bearer {remote_user.access_token}"})
+                self.headers.update(
+                    {"Authorization": f"Bearer {remote_user.access_token}"}
+                )
 
                 # Resend the request
                 r.request.refresh_done = True  # prevent infinite recursion
@@ -36,4 +44,4 @@ class OAuth2Session(Session):
         super().__init__(**kwargs)
 
         self.headers.update({"Authorization": f"Bearer {remote_user.access_token}"})
-        self.hooks['response'].append(update_token_on_request)
+        self.hooks["response"].append(update_token_on_request)
--- nens_auth_client/tests/test_requests_session.py
+++ nens_auth_client/tests/test_requests_session.py
@@ -1,54 +1,68 @@
 from nens_auth_client.models import RemoteUser
 from nens_auth_client.requests_session import OAuth2Session
+from unittest import mock
 from urllib.parse import parse_qs
+
 import pytest
 
 
 @pytest.fixture
 def remote_user(access_token_generator):
-    return RemoteUser(
+    remote_user = RemoteUser(
         access_token=access_token_generator(),
         refresh_token="foo",
     )
+    with mock.patch.object(remote_user, "save"):
+        yield remote_user
 
 
 def test_no_refresh(rq_mocker, remote_user):
     session = OAuth2Session(remote_user)
 
+    # Mock an API
     rq_mocker.get("http://api.foo.bar", json={"data": "Hello World!"}, status_code=200)
+
     response = session.get("http://api.foo.bar")
     assert response.json() == {"data": "Hello World!"}
+
+    # Expect 1 request in the request history
+    request_list = rq_mocker.request_history
+    assert len(request_list) == 1
+    assert (
+        request_list[0].headers["Authorization"] == f"Bearer {remote_user.access_token}"
+    )
 
 
 def test_refresh(rq_mocker, openid_configuration, remote_user):
     valid_token = remote_user.access_token
-    remote_user.access_token = "Invalid"
+    remote_user.access_token = "some-invalid-token"
 
     session = OAuth2Session(remote_user)
 
-    rq_mocker.get(
-        "http://api.foo.bar/",
-        status_code=401
-    )
+    rq_mocker.get("http://api.foo.bar/", status_code=401)
     rq_mocker.post(
-        openid_configuration["token_endpoint"], json={"id_token": "foo", "access_token": valid_token, "refresh_token": "bar"}
+        openid_configuration["token_endpoint"],
+        json={"id_token": "foo", "access_token": valid_token, "refresh_token": "bar"},
     )
 
     session.get("http://api.foo.bar/")
-    
-    # expect 4 requests:
-    a, b, c, d = rq_mocker.request_history
+
+    # Expect 4 requests in the request history
+    request_list = rq_mocker.request_history
+    assert len(request_list) == 4
+
     # Initial request
-    assert rq_mocker.request_history[0].url == "http://api.foo.bar/"
+    assert request_list[0].url == "http://api.foo.bar/"
 
     # Auth server auto discovery (tested elsewere)
-    assert "openid-configuration" in rq_mocker.request_history[1].url
+    assert "openid-configuration" in request_list[1].url
 
-    # Refresh token
-    assert rq_mocker.request_history[2].url == openid_configuration["token_endpoint"]
-    qs = parse_qs(rq_mocker.request_history[2].text)
+    # Refresh token fetch
+    assert request_list[2].url == openid_configuration["token_endpoint"]
+    qs = parse_qs(request_list[2].text)
     assert qs["grant_type"] == ["refresh_token"]
     assert qs["refresh_token"] == [remote_user.refresh_token]
 
     # Request with refreshed token
-    assert rq_mocker.request_history[3].url == "http://api.foo.bar/"
+    assert request_list[3].url == "http://api.foo.bar/"
+    assert request_list[3].headers["Authorization"] == f"Bearer {valid_token}"
