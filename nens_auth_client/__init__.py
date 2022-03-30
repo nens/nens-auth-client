183f9762ed0b9ac7f757f0fd185e7f2446a334df
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2022-03-30 20:26:59 +0200

    Silence django deprecation warnings

--- nens_auth_client/__init__.py
+++ nens_auth_client/__init__.py
@@ -1,2 +1,4 @@
-# package
-default_app_config = "nens_auth_client.apps.NensAuthClientConfig"
+from django import VERSION
+
+if VERSION[0] < 4:
+    default_app_config = "nens_auth_client.apps.NensAuthClientConfig"
