2b822a1b994d8d7bb6e82e3c2d489b3db38c265e
byrman <carsten.byrman@nelen-schuurmans.nl>
2026-03-25 13:06:00 +0100

    Save WIP

--- nens_auth_client/views.py
+++ nens_auth_client/views.py
@@ -342,17 +342,17 @@ class WelcomeView(TemplateView):
 
 
 class RegistrationForm(forms.Form):
-    first_name = forms.CharField(max_length=5)
-    last_name = forms.CharField()
-    password = forms.CharField()
-    password2 = forms.CharField()
+    first_name = forms.CharField(max_length=150)  # Limited by Django
+    last_name = forms.CharField(max_length=150)  # Limited by Django
+    password = forms.CharField(max_length=256)  # Limited by Cognito
+    password2 = forms.CharField(max_length=256)  # Limited by Cognito
 
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data["password"]
         password2 = cleaned_data["password2"]
         if password != password2:
-            raise ValidationError("Passwords do not match")
+            raise ValidationError("Passwords do not match.")
         return cleaned_data
 
     def create_cognito_account(self):
